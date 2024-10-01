6d893bda41d32b6a49db7f1c1142a28550c88ffe
airondev <airondev@gmail.com>
2024-10-01 10:35:33 +0100

    fix

--- src/engines/google.js
+++ src/engines/google.js
@@ -1,8 +1,8 @@
-const base = "https://translate.googleapis.com/translate_a/single";
-
+// const base = "https://translate.googleapis.com/translate_a/single";
+const base = `https://translation.googleapis.com/language/translate/v2`;
 export default {
-  fetch: ({ key, from, to, text }) => [
-    `${base}?client=gtx&sl=${from}&tl=${to}&dt=t&q=${encodeURI(text)}`
+  fetch: ({ key, from, to, text, format }) => [
+    `${base}?key=${key}&source=${from}&target=${to}&format=${format}&q=${encodeURI(text)}`
   ],
   parse: res =>
     res.json().then(body => {
